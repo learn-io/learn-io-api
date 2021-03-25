86dc84546c5cba88befa24eab3dc4ddc836d8038
xin0415 <34253658+xin0415@users.noreply.github.com>
2021-03-25 00:18:27 -0400

    test
    connect to mongodb

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
+const MongoClient = require('mongodb').MongoClient;
 const express=require('express');
-var mongo=require('mongodb');
 
+const url='mongodb://localhost:27017';
+const dbName='learnio';
 const app=express();
+
+MongoClient.connect(url,function(err,db){
+	if(err) throw err;
+	var dbo=db.db(dbName);
+	dbo.collection("userInfo").find().toArray(function(err,result){
+		if(err) throw err;
+		console.log(result);
+		db.close();
+	})
+});
+
