d06b749d88ba7225ec0c2f2c87f3bd3711d7ffb7
DanielDeLayo <dandelayo@gmail.com>
2021-04-04 14:09:48 -0400

    Fixing example test suite. works now

--- test/basic.js
+++ test/basic.js
@@ -1,13 +1,22 @@
 var expect = require("chai").expect;
 
 describe("This is an example test!", function() {
-    describe("This one always passes", function(){
-        expect(1).to.equal(1);
-        expect(5).to.equal(5);
+    context('words', function() {
+        it("always passes", function(){
+            expect(1).to.equal(1);
+            expect(5).to.equal(5);
+        });
+        it("also always passes", function(){
+            expect(4).to.equal(4);
+            expect(566).to.equal(566);
+        });
     });
-    describe("This one also always passes", function(){
-        expect(2).to.equal(2);
-        expect([1,2]).to.deep.equal([1,2]);
-        //test change!
+    context('more words', function()
+    {
+        it("also also always passes", function(){
+            expect(2).to.equal(2);
+            expect([1,2]).to.deep.equal([1,2]);
+            //test change!
+        });
     });
 });
