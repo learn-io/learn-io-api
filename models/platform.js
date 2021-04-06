a45b6dde0d56b64ce6cfb0d94e341085c847dd19
Akshay Karve <akshay.karve@stonybrook.edu>
2021-04-06 11:53:56 -0400

    Changed models for page and platform js to match dataModel

--- models/platform.js
+++ models/platform.js
@@ -1,14 +1,15 @@
 const mongoose=require('mongoose')
 
 const platformSchema=new mongoose.Schema({
-	platformName:{type:String,required:true,unique:true},
+	name:{type:String,required:true,unique:true},
+	id:{type:String,required:true,unique:true},
 	image:{type:String,default:""},
 	description:{type:String,default:""},
-	owner:{type:String, required:true},
 	modules:[{
-		pageName:{type:String},
-		pageDescription:{type:String},
+		moduleName:{type:String},
+		moduleDescription:{type:String},
 		image:{type:String}
-	}]
+	}],
+	owner:{type:String, required:true}
 });
 module.exports=mongoose.model('platform' ,platformSchema,'platform');
